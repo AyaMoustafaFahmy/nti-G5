aece1f4644a687f052fd161de6785d8093569055
marwa Radwan <marwaradwan872@hotmail.com>
2021-03-03 11:23:56 +0200

    s4

--- session4/app.js
+++ session4/app.js
@@ -1,10 +1,13 @@
 //npm init =>initialize npm 
 //npm i pkg
 //npm i
+//npm i -g nodemon
+//nodemon app.js
+
 
 const myFs = require('fs')
-myFs.writeFileSync('test.js', 'function hello(){console.log(\'say hello\')};module.exports={hello}')
+// myFs.writeFileSync('test.js', 'function hello(){console.log(\'say hello\')};module.exports={hello}')
 const _play = require('./test.js')
-_play.hello()
+// _play.hello()
 const validator = require('validator')
-console.log(validator.isAlphanumeric('jnbji$^^hhj654gh'))
+console.log(validator.isAlphanumeric('jhgbv66'))
