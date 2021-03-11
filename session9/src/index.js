024fdcbcf02813b55e8c6d6dee63ae2f06c71925
marwa Radwan <marwaradwan872@hotmail.com>
2021-03-11 11:37:49 +0200

    s9

--- session9/src/index.js
+++ session9/src/index.js
@@ -7,4 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(userRoutes)
 app.use(bookRoutes)
+// const bcryptjs = require('bcryptjs')
+// async function x(){
+//  let pass = '123456'
+//  z = await bcryptjs.hash(pass, 12)
+//  console.log(z)
+//  ismatch = await bcryptjs.compare(pass, z)
+//  console.log(ismatch)
+// }
+// x()
 app.listen(3000)
--- session9/src/models/user.model.js
+++ session9/src/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
+const bcrypt = require('bcryptjs')
 const userSchema = mongoose.Schema({
     name:{
         type:String,
@@ -33,6 +33,13 @@ const userSchema = mongoose.Schema({
         type: Boolean,
         default:false
     }
+})
+userSchema.pre('save', async function(next){
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 12)
+    }
+    next()
 })
 const User = mongoose.model('User', userSchema)
 module.exports=User
