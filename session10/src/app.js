65ab1f2b6e34e08900d95b454ef86a071a352236
Marwa Radwan <marwaradwan872@hotmail.com>
2021-03-14 13:06:53 +0200

    s10

--- session10/src/app.js
+++ session10/src/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 require('./db/mongoose')
 const app = express()
-// const userModel = require('./models/user')
-// x=new userModel({
-//     fname:'marwa', lname:'radwan', phone:"011111111",country:'ggg'
-// })
-// x.save().then(()=>{}).catch(e=>{console.log(e)})
+const userModel = require('./models/user')
+x=new userModel({
+    fname:'marwa', 
+    lname:'radwan', 
+    phone:"01234567891",
+    country:'iraq', 
+    password:"123456", 
+    email:"test@test.com",
+    username:"xyz1"
+})
+x.save().then(()=>{}).catch(e=>{console.log(e)})
 app.use(express.json())
 
 module.exports = app
--- session10/src/models/user.js
+++ session10/src/models/user.js
@@ -1,15 +1,3 @@
-// id => auto increament
-// fname
-// lname
-// phone => optional
-// country => from list
-// password
-// image
-// address array of address
-// birthdate
-// email
-// username
-// friends [userid]
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -33,18 +21,52 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         trim:true
+    },
+    birthdate:{
+        type:Date
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate(value){
+            if(!validator.isEmail(value)) throw new Error('invalid email')
+        }
+    },
+    username:{
+        type:String,
+        unique:true
+    },
+    addresses:[{
+        address: {
+            addrType:{type:String}, 
+            addrDetails:{type:String}
+        }
+    }],
+    friends:[{
+        type:mongoose.Schema.Types.ObjectId
+    }],
+    tokens:[
+        {token:{type:String, required:true}}
+    ],
+    userProfile:{
+        type:String
     }
-})
-
+},{
+    timestamps:true
+}
+)
 userSchema.pre('save', async function(next){
     lastUser = await User.findOne({}).sort({_id:-1})
     user = this
+    if(!user.username)  user.username =user._id
+    
     //check on update
     if(!lastUser) user.userId=1
     else user.userId = lastUser.userId+1    
     
-    if(user.isModified(password)){
-        user.password = await bcrypt(user.password, 12)
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 12)
     }
     next()
 })
