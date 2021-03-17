19fd585959b314da726310f0377f8eded089818c
marwa Radwan <marwaradwan872@hotmail.com>
2021-03-17 10:40:32 +0200

    s13

---
+++ session13/play/index.ts
@@ -0,0 +1,19 @@
+class User{
+    name:string
+    age: number
+    //constructor
+    constructor(name: any=null, age:any=null){
+        console.log('hello')
+    }
+    
+}
+let marwa = new User()
+marwa.name="marwa"
+marwa.age=36
+
+//overloading
+//override
+//polymorphism
+
+
+
