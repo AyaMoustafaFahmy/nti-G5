90f928f5eaa9a8d70b26cf9c873f26b9385b76af
marwa Radwan <marwaradwan872@hotmail.com>
2021-03-18 11:32:52 +0200

    s14

---
+++ session14/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'session14';
+  // myClass = 'x'
+  // myImg = '/assets/1.jpeg'
+  // cols=3
+  // x = '<h2>hello i\'m x</h2>'
+  // classN = 'a'
+  // test(){return 'hello from ts'}
+  // changeMe(){
+  //   this.classN=='a'? this.classN='b' : this.classN='a'
+  // }
+//   data = "show"
+//   formClass ="dnone"
+//   showHideForm(){
+//     this.data=='show'? this.data="hide": this.data="show" 
+//     this.formClass=='dnone'? this.formClass='dblock': this.formClass='dnone'
+//   }
+//   test = ''
+//   handelInput(d){
+// console.log(d.target.value)
+// this.test = d.target.value
+//   }
+myData = ''
+}
