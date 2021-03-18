8dd92641de6642e815c0a37728fe872ae752f706
marwa Radwan <marwaradwan872@hotmail.com>
2021-03-18 13:02:54 +0200

    s14

--- session14/src/app/app.module.ts
+++ session14/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
